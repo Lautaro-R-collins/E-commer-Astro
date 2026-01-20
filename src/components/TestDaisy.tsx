64e8b5421b102de81d9d9fa8076b23927b7defcd
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-19 21:41:09 -0300

    add test component

---
+++ src/components/TestDaisy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestDaisy = () => {
+  return (
+    <div className="p-10 flex flex-col items-center gap-4">
+      <button className="btn btn-primary">Botón de DaisyUI</button>
+    </div>
+  );
+};
+
+export default TestDaisy;
