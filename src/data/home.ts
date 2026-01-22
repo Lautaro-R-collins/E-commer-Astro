80e50220086c7faf61cbd9a1c0fa3809921aa523
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-21 21:28:13 -0300

    add data home

---
+++ src/data/home.ts
@@ -0,0 +1,45 @@
+export const categories = [
+  {
+    name: "Computers",
+    image:
+      "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?auto=format&fit=crop&w=500&q=80",
+    path: "/category/computers",
+  },
+  {
+    name: "Phones",
+    image:
+      "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?auto=format&fit=crop&w=500&q=80",
+    path: "/category/phones",
+  },
+  {
+    name: "Audio",
+    image:
+      "https://images.unsplash.com/photo-1558403194-611308249627?auto=format&fit=crop&w=500&q=80",
+    path: "/category/audio",
+  },
+  {
+    name: "Accessories",
+    image:
+      "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?auto=format&fit=crop&w=500&q=80",
+    path: "/category/accessories",
+  },
+];
+
+export const featuredLinks = [
+  {
+    name: "About Us",
+    image:
+      "https://images.unsplash.com/photo-1522071820081-009f0129c71c?auto=format&fit=crop&w=800&q=80",
+    path: "/about",
+    className: "lg:col-span-2",
+    imageClass: "aspect-square lg:aspect-[2/1]",
+  },
+  {
+    name: "Our Blog",
+    image:
+      "https://img.freepik.com/foto-gratis/mensaje-online-blog-chat-comunicacion-envolver-concepto-icono-grafico_53876-139717.jpg?semt=ais_hybrid&w=740&q=80",
+    path: "/blog",
+    className: "lg:col-span-2",
+    imageClass: "aspect-square lg:aspect-[2/1]",
+  },
+];
